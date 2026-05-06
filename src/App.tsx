ad846dd12a34648d7ff2fc3f736af48aed254fdb
Sulochan Mahajan <sulochanmahajan@gmail.com>
2026-05-06 19:24:49 +0530

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchMeals } from "./utils/meals";
 import type { Meal } from "./types/meal";
 import MealCard from "./components/MealCard";
